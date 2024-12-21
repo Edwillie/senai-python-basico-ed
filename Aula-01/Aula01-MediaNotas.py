3a8df1ac7e2dd1b8ada216017e538ce7f0c0e3bc
Edwillie Cardoso <edwillie@gmail.com>
2024-12-21 16:55:19 -0300

    final da aula 01

---
+++ Aula-01/Aula01-MediaNotas.py
@@ -0,0 +1,9 @@
+passou = input('Será que você merece presente de natal?\nVocê Passou de ano?\n(Sim / Não): ')
+nota1 = float(input('Qual foi sua 1a nota?\n'))
+nota2 = float(input('Qual foi sua 2a nota?\n'))
+nota3 = float(input('Qual foi sua 3a nota?\n'))
+nota4 = float(input('Qual foi sua 4a nota?\n'))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+
+print(f'Sua média foi {media}!!!')
