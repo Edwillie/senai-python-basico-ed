8bc7edd9a9182e5fdbf37b76d90050b1b0687624
Edwillie Cardoso <edwillie@gmail.com>
2024-12-28 16:45:52 -0300

    exercicio 1 da aula 2

---
+++ Aula-02/Aula02-CalculoConsumo.py
@@ -0,0 +1,24 @@
+import os
+os.system('cls')
+
+consumo = input('Informe o consumo em WATTS: ')
+
+if not consumo=='':
+    print(f'O tipo da variavel de consumo, ANTES da conversão é: {type(consumo)}')
+
+    consumo = float(consumo)
+    print(f'O tipo da variavel de consumo, DEPOIS da conversão é: {type(consumo)}')
+
+    if consumo <= 100:
+        tarifa = 0.65
+    elif consumo <= 150:
+        tarifa = 0.70
+    elif consumo <= 200:
+        tarifa = 0.75
+    else:
+        tarifa = 0.80        
+
+    valorconsumido = consumo * tarifa        
+    print(f'O valor calculado foi de R${valorconsumido:.2f}. Onde a tarifa foi de R${tarifa:.2f} devido ao consumo de {consumo} WATTS ')    
+else:
+    print('O valor de consumo em WATTS é requerido')
