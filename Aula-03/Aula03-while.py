df2f3b5924a69bc0f398c722f11aba8aa1b8bb79
Edwillie Cardoso <edwillie@gmail.com>
2025-01-04 16:13:19 -0300

    Jogo 300

--- Aula-03/Aula03-while.py
+++ Aula-03/Aula03-while.py
@@ -1,10 +1,22 @@
-contador = 0
+import os
+import random
 
-while True: #Executa enquanto a condição for verdadeira!
-    print(f'Contador em {contador}')
+os.system('cls')
+numero_secreto = random.randrange(0, 300000)
+tentativas = 0
 
-    resp = input('Continuo contando (Sim / Não)? ')
-    if resp.upper() == 'S' or resp.upper() == 'SIM':
-        contador += 1
-    else:
+while tentativas < 5: #Executa enquanto a condição for verdadeira!
+    tentativas += 1
+    resp = int(input('Meu caro Tidas, diga um número de com quantos persas vamos lutar? '))
+
+    if resp > numero_secreto:
+        print('Errou! Um pouco menos!')
+    elif resp < numero_secreto:
+        print('Errou! Um pouco mais')
+    elif resp == numero_secreto:
+        print(f'Sim Tidas! Lutaremos contra {numero_secreto} persas! Ahul!')
         break
+
+else:
+    print(f'Tidas, lutaremos contra {numero_secreto} persas! Ahul!')
+    print('Ohhh Leonidas... Tu tem que parar de arrumar essas tretas!')    
