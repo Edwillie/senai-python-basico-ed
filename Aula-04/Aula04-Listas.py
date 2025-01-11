d4fd96ecc3356d52c3103caf07c2ca306a75f784
Edwillie <edwillie@gmail.com>
2025-01-11 16:08:59 -0300

    Exercicio de Lista concluido

--- Aula-04/Aula04-Listas.py
+++ Aula-04/Aula04-Listas.py
@@ -1,21 +1,60 @@
 #Bibliotecas
 import os
+import time
 
 #Variaveis
 listaNomes = []
-listaIndices = []
 indiceNomes = 0
 
+#Coleta os nomes de Pessoas
 while True:
     os.system('cls')
     nome = input('Insira um nome de uma pessoa: (ou pressione "Q", caso queira parar de inserir nomes) ')
 
     if nome.upper() == "Q":
         break
+    elif nome.upper() == "NÃO" or nome.upper() == "SAIR":
+        print('ATENÇÃO: Você usou palaras reservadas!')
+        time.sleep(5)
     else:
-        indiceNomes += 1
-        listaIndices.append(indiceNomes)
         listaNomes.append(nome)
 
-print('Aqui está sua lista')
-print(listaIndices, listaNomes)
+#Lista o nome das pessoas coletadas
+os.system('cls')
+print('Aqui está sua lista:')
+for nome in listaNomes: 
+    indiceNomes += 1
+    print(f'{indiceNomes:02} : {nome}')
+
+#Atua com o usuario para corrigir a lista
+print('\n')
+while True:
+    NomeOrig = input('Há algum nome para corrigir na lista? Digite o nome a ser corrigido ("NÃO" ou "SAIR" ou "Q", para encerrar): ')
+
+    #Verifica se o usuario informou um nome ou se deseja sair do programa
+    if NomeOrig.upper() == "NÃO" or NomeOrig.upper() == "SAIR" or NomeOrig.upper() == "Q":
+       break
+    else:
+        novoNome = input('Informe o novo nome: ') 
+
+    #Verifica se o mesmo nome se repete na lista, e questiona qual o nome correto para ser corrigido
+    if listaNomes.count(NomeOrig) > 1:
+       indiceAlt = int(input(f'Existe mais de um {NomeOrig} na lista! Informe o numero do qual você quer alterar: ')) - 1
+    else:
+       indiceAlt = listaNomes.index(NomeOrig)
+
+    #Corrige o nome na lista
+    if indiceAlt >= 0 and indiceAlt <= len(listaNomes):
+        listaNomes[indiceAlt] = novoNome       
+    else:
+        break    
+
+    #Volta a imprimir a lista, para reiniciar o processo.
+    os.system('cls')
+    print('Aqui está sua lista (corrigida):')
+    indiceNomes = 0
+    for nome in listaNomes: 
+        indiceNomes += 1
+        print(f'{indiceNomes:02} : {nome}')
+
+    print('\n')
